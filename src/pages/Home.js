8da0ba523243b13733c51b800b4c21a58137a278
JimmyCamus <jeremy.camus.varela@gmail.com>
2022-11-23 14:56:32 -0300

    feat: redirection to login at home.

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,16 @@
+import { useNavigate } from "react-router-dom";
 import { useLogOut } from "../hooks/logout.hook";
 import { useUser } from "../hooks/user.hook";
 
 const HomePage = ({ ctxUser }) => {
   const userContext = useUser();
   const handleLogOut = useLogOut;
+  const redirect = useNavigate();
+
+  if (!ctxUser.email) {
+    redirect("/login");
+  }
+
   return (
     <div>
       <h3>User logged in: {ctxUser ? ctxUser.email : null}</h3>
